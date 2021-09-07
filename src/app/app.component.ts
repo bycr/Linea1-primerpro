bf46d061257ecc5e441b969fc3c1f51ac2a1f93f
Brayan cañon <brayan13canon@gmail.com>
2021-09-07 08:31:19 -0500

    conectando componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,37 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'pri-proy';
+  numero1 : number;
+  numero2 : number;
+  resultado : number;
+
+  public operar(op: string):void{
+
+    switch(op){
+      case 's': 
+        this.resultado = this.numero1 + this.numero2;
+        break;
+      case 'r':
+        this.resultado = this.numero1 - this.numero2;
+        break;
+      case 'm':
+        this.resultado = this.numero1 * this.numero2;
+        break;
+        case 'd':
+          this.resultado = this.numero1 / this.numero2;
+          break; 
+        default:
+           this.resultado = 0;
+    }
+  }
+    
+
+  public teclear(e: any):void{
+      console.log("la tecla que se oprimio fue: "+e.key);
+      console.log("la palabra al momento es: "+e.target.value)
+  }
+  
 }
